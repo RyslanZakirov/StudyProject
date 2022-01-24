7c50e2fa03a85045573b4b700793edafd6c96821
Ruslan <rus.zakirov.2000@bk.ru>
2022-01-24 19:36:39 +0700

    Установка размера 2д полотна

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const canvas = document.querySelector("#canvas")
     const ctx = canvas.getContext("2d")
 
+    canvas.width = document.body.offsetWidth
+    canvas.height = document.body.offsetHeight
+
     console.log(ctx)
 
 })
